79d2786db394f92344c85445995aa52e19a52d15
nas <nnagorngar@gmail.com>
2022-07-26 19:56:30 +0100

    2eme commit

--- badec_app/urls.py
+++ badec_app/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('reservationsalle/', views.reservationsalle, name='reservationsalle'),
+    path('paiement/', views.paiement, name='paiement')
+
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- badec_app/views.py
+++ badec_app/views.py
@@ -1,10 +1,15 @@
+from click import style
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'main.html')
+    return render(request,'style/accueil.html')
 
 
-def reservation(request):
-    return render(request,'reservationsalle.html')
+def reservationsalle(request):
+    return render(request,'style/reservationsalle.html')
+
+
+def paiement(request):
+    return render(request, 'style/paiement.html')
